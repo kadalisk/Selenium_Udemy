37c5a07a81c9b228e46de1f0d75934abfde8f06d
Suresh Kumar Kadali <kadalisk@gmail.com>
2016-04-19 20:34:44 -0400

    Added Selenium Project
    Added Selenium Project

---
+++ JavaPractise/src/suresh/Basics1.java
@@ -0,0 +1,14 @@
+package suresh;
+
+public class Basics1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//This step prints in the output
+		
+		System.out.println("I started Java Learning");
+
+	}
+
+}
---
+++ JavaPractise/src/suresh/Methods.java
@@ -0,0 +1,15 @@
+package suresh;
+
+public class Methods {
+	
+public int ValidateHeader(){
+	
+	System.out.println("Header links Validated");
+	
+	return 2;
+	
+	
+	
+}
+
+}
